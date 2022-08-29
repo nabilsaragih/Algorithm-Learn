4f416e41f7bfa637228d19327e8e27469655f7b6
nabilsaragih <nabilsaragih33@gmail.com>
2022-08-29 19:05:04 +0800

    adding tlx toki

---
+++ TLX TOKI/nilai.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+if n >= 0 and n <= 100:
+    print("YA")
+else:
+    print("TIDAK")
---
+++ TLX TOKI/operasi.py
@@ -0,0 +1,9 @@
+n = list(input())
+n.pop(1)
+n = list(map(int, n))
+
+print(n[0] + n[1])
+print(n[0] - n[1])
+print(n[0] * n[1])
+print(n[0] / n[1])
+print(n[0] % n[1])
